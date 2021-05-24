cba6a8e79f85407f81752b84d2165ad41456ba72
Emanuel Lucas Mizael dos Santos <79867442+mizaelucas@users.noreply.github.com>
2021-05-24 00:00:43 -0300

    Adicionado método PrivateRoute para impedir acesso a páginas sem login

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,6 +5,7 @@ import { AuthProvider } from '../../Contexts/AuthContext';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Dashboard from '../Dashboard/Dashboard';
 import Login from '../Login/Login';
+import PrivateRoute from '../PrivateRoute';
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
                 <Route path="/signup" component={SignUp} />
                 <Route path="/login" component={Login} />
               </div>
-                <Route exact path="/" component={Dashboard} />
+                <PrivateRoute exact path="/" component={Dashboard} />
           </div>
         </Switch>
       </AuthProvider>
