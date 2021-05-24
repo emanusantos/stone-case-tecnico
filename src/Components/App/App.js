4301d2e4de98b0481dc65784e2f9e40fa98f3b0d
Emanuel Lucas Mizael dos Santos <79867442+mizaelucas@users.noreply.github.com>
2021-05-23 21:36:49 -0300

    Adicionado o componente Navbar

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,6 +5,7 @@ import { AuthProvider } from '../../Contexts/AuthContext';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Dashboard from '../Dashboard/Dashboard';
 import Login from '../Login/Login';
+import Navbar from '../Navbar/Navbar';
 
 function App() {
   return (
@@ -16,7 +17,10 @@ function App() {
                 <Route path="/signup" component={SignUp} />
                 <Route path="/login" component={Login} />
               </div>
+              <>
+                <Navbar />
                 <Route exact path="/" component={Dashboard} />
+              </>
           </div>
         </Switch>
       </AuthProvider>
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.css';
+
+export default function Navbar() {
+    return (
+        <nav>
+            <h2>StoneTest</h2>
+            <ul>
+                <li>Página inicial</li>
+                <li>Comics</li>
+                <li>Characters</li>
+                <li>Favoritos</li>
+                <li>Meu perfil</li>
+            </ul>
+        </nav>
+    )
+}
