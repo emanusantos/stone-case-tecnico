1d512a58a1f6aa9c7ce1498d16188544c5ea047b
Emanuel Lucas Mizael dos Santos <79867442+mizaelucas@users.noreply.github.com>
2021-05-24 18:07:17 -0300

    Adicionado componente Comics (debug necessário)

--- src/Components/Comics/Comics.js
+++ src/Components/Comics/Comics.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Navbar from '../Navbar/Navbar';
 
 export default function Comics() {
+
+    const publickey = 'ebdeb55d53ecc6709426269c1331c663'
+    const timestamp = '1621881645'
+    const md5 = 'ef7a9359ab0278556690718d66cf527c'
+
+        useEffect(() => {
+            fetchItems();
+        }, []);
+
+    const [comics, setComics] = useState([]);
+
+    const fetchItems = async () => {
+        const data = await fetch(
+            'http://gateway.marvel.com/v1/public/comics?ts=1621881645&apikey=ebdeb55d53ecc6709426269c1331c663&hash=ef7a9359ab0278556690718d66cf527c'
+        );
+        const items = await data.json();
+        console.log(items.data.results);
+        setComics(items.data.results);
+    }
     return (
         <div>
             <Navbar />
-            Comics
+            {comics.map(item => (
+                <div>
+                    <img key="img" src={`${item.images.path}.${item.images.extension}`} alt=""/>
+                    <h1 key="title">{item.title}</h1>
+                    <p key="description">{item.description}</p>
+                </div>
+            ))}
         </div>
-    )
-}
+
+    )}
