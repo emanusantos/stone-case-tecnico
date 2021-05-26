b906ba5d351e78be6f7e15adbf6be3738ca7f2d3
Emanuel Lucas Mizael dos Santos <79867442+mizaelucas@users.noreply.github.com>
2021-05-26 18:17:25 -0300

    Adicionada lógica para não acessar rota padrão '/'

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import SignUp from '../SignUp/SignUp.jsx'
 import { AuthProvider } from '../../Contexts/AuthContext';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Profile from '../Profile/Profile';
 import Login from '../Login/Login';
 import PrivateRoute from '../PrivateRoute';
@@ -19,6 +19,8 @@ function App() {
                 <Route path="/signup" component={SignUp} />
                 <Route path="/login" component={Login} />
               </div>
+              <Route exact path='/' render={() =>
+              (<Redirect to="/profile"/>)} />
                 <PrivateRoute exact path="/profile" component={Profile} />
                 <PrivateRoute exact path="/comics" component={Comics} />
                 <PrivateRoute exact path="/characters" component={Characters} />
